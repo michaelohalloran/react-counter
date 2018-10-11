a73bb1791129754a36f774341ff14a617ea14543
Michael O'Halloran <C-Michael.OHalloran@charter.com>
2018-10-11 17:24:20 -0600

    Simply React counter with property initializer arrow functions

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
+// https://www.fullstackreact.com/articles/use-property-initializers-for-cleaner-react-components/
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+
+  state = {
+    count: 0,
+    errorMsg: ''
+  };
+
+  componentDidUpdate() {
+    //if the count is positive and there's still an error, clear it
+    return this.state.count > 0 && this.state.errorMsg ? 
+      this.setState({errorMsg: ''}) : 
+      null;
+  }
+
+  incrementCount = ()=> {
+    this.setState((prevState)=> {
+      return {count: prevState.count + 1}
+    });
+  }
+
+  decrementCount = ()=> {
+    if(this.state.count > 0) {
+      this.setState(prevState=> (
+            {count: prevState.count - 1}
+          )
+      );
+    } else {
+      this.setState({errorMsg: 'Negative numbers not allowed'})
+    }
+  }
+
   render() {
+
+    let testText;
+    testText = this.state.count > 5 ? 
+      <div>Count now > 5</div> : 
+      null
+      ;
+      
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+        <h1>Count: </h1>
+        {/* <h2 style={{background: 'red', width: '50px'}>
+          {this.state.count}
+        </h2> */}
+        <h2 className="h2">{this.state.count}</h2>
+        <div className="btn" onClick={this.incrementCount}>+</div>
+        <div className="btn" onClick={this.decrementCount}>-</div>
+        {this.state.errorMsg && <div>{this.state.errorMsg}</div>}
+        {testText}
       </div>
     );
   }
